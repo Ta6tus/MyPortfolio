893936726a4ba81404c50c673e0d92a59e1ed31e
Tacitus <micpalokouna@gmail.com>
2023-10-28 16:11:09 +0000

    ajout du repertoire

---
+++ main.js
@@ -0,0 +1,75 @@
+var typed= new Typed(".text",{
+    strings:["Security Engineer" , "Web Developer" , "Graphic Designer", "Video Editor", "Youtuber"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+});
+
+/*const section = document.querySelector(".pop-up"),
+        overlay = document.querySelector(".overlay"),
+        showBtn = document.querySelector(".butonHIDS"),
+        closeBtn = document.querySelector(".close-btn");
+
+      showBtn.addEventListener("click", () => pop-up.classList.add("active"));
+
+      overlay.addEventListener("click", () =>
+        section.classList.remove("active")
+      );
+
+      closeBtn.addEventListener("click", () =>
+        section.classList.remove("active")
+      );
+
+/*
+const popUp = document.querySelector(".pop-up");
+const overlay = document.querySelector(".overlay");
+const showBtn = document.querySelector(".butonHIDS");
+const closeBtn = document.querySelector(".close-btn");
+
+showBtn.addEventListener("click", () => popUp.classList.add("active"));
+
+overlay.addEventListener("click", () => popUp.classList.remove("active"));
+
+closeBtn.addEventListener("click", () => popUp.classList.remove("active"));
+*/
+
+/*function sendMail() {
+  const form = document.getElementById("myForm");
+
+  // Récupérez les valeurs des champs du formulaire
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  // Créez un objet avec les données du formulaire
+  const params = {
+    name: name,
+    email: email,
+    subject: subject,
+    message: message
+  };
+
+  /*if (!form.checkValidity()) {
+    alert("Veuillez remplir tous les champs du formulaire.");
+    return;
+  }
+
+  emailjs.init("M4LymWqEYOacL436d"); // Remplacez "user_your_user_id" par votre ID EmailJS
+
+  emailjs.sendForm("service_5j21pit", "template_yh195hr", params)
+      .then(function(response) {
+          console.log("E-mail sent succesfully", response);
+          alert("Email Sent Succesfully !")
+          form.reset(); // Réinitialise le formulaire
+      }, function(error) {
+          console.error("Error", error);
+          alert("Error !!!");
+      });
+}*/
+
